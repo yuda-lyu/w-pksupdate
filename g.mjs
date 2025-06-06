69c3ebeec6f728a45ca4dcbf5081a3d99d47689b
yuda-lyu <firsemisphere@gmail.com>
2025-06-06 14:25:03 +0800

    modify: ext. names

--- g.mjs
+++ g.mjs
@@ -5,15 +5,25 @@ import publishPackages from './src/publishPackages.mjs'
 
 // let names = []
 let names = [
+    //雖然是無限查找有更新就更新:
+    // 但有些是devDependencies依賴前者套件, 至少須給與相同level, 若dependencies依賴前者套件就一定要給予大於level
+    // 因devDependencies有出現新版依然會更新與發布, 即便非必要, 所以names盡量給予level由小至大的順序, 減少自動化時非必要之更新發布
+
     'w-data-csv',
     'w-dwdata-binance',
     'w-data-syncer',
+
     'w-converhp',
     'w-serv-broadcast',
     'w-serv-orm',
     'w-sync-webdata',
     'w-serv-webdata',
     'w-serv-hapi',
+
+    'w-web-api',
+    'w-web-perm',
+    'w-web-sso',
+
 ]
 
 await publishPackages(names)
